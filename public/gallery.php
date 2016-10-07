58d349e9a64eb2bd0de3d8964d1129b02ea69ea1
across <crossal@tcd.ie>
2016-10-07 14:57:10 +0100

    makeover - bootstrap

--- public/gallery.php
+++ public/gallery.php
@@ -3,90 +3,263 @@
 <head>
 	<title>Aideen Cross Designs</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<link href="css/style.css" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Alex+Brush" />
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	
 	<script src="js/image_slider.js" type="text/javascript"></script>
+	
+	<!-- Blueimp image gallery CSS -->
+	<link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+	<script src="//blueimp.github.io/Gallery/js/jquery.blueimp-gallery.min.js"></script>
+	<script src="js/bootstrap-image-gallery.min.js"></script>
+	
+	<!-- Image gallery CSS -->
+	<link rel="stylesheet" href="css/bootstrap-image-gallery.min.css">
+	
+	<!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i|Pinyon+Script|Open+Sans|Alex+Brush" rel="stylesheet">
+	
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	<div class="wrapperBody">
 	
 		<header>
-			<!--<h1>Aideen Cross Designs</h1>-->
-			<div class="banner">
-				<img src="images/logo.jpg" alt=""/>
-			</div>
-			<nav>
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a style="text-decoration: underline; color: #ff3399" href="gallery.php">Gallery</a></li>
-					<li><a href="collections.php">Collections</a></li>
-					<li><a href="our_brides.php">Our Brides</a></li>
-					<li><a href="flowergirl_communion.php">Flowergirl/Communion</a></li>
-					<li><a href="about.php">Meet Aideen</a></li>
-					<li><a href="contact.php">Contact Us</a></li>
-				</ul>
-			</nav>
-		</header>
+			<nav class="navbar navbar-default navbar-static-top tackNav">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar6">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Aideen Cross Designs <span class="sub-brand">Award Winning Bridal Designer</span></a>
+						
+					</div>
+					<div id="navbar6" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="index.php">Home</a></li>
+							<li><a href="about.php">About</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li class="active"><a href="#">Gallery</a></li>
 
-		<hr>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<!--end new nav bar-->
+		</header>
 		
 		<div class="core">
-			<div class="gallery">
-				<ul>	
-					<li>
-						<img src="images/gallery/collection_niamh.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_11.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/collection_aoife.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/collection_carol.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/collection_louise.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_12.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_13.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_1.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_2.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_3.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_4.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_5.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_6.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_7.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_8.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_9.jpg" alt=""/>
-					</li>
-					<li>
-						<img src="images/gallery/gallery_10.jpg" alt=""/>
-					</li>
-				</ul>  
+			<div id="blueimp-gallery" class="blueimp-gallery">
+				<!-- The container for the modal slides -->
+				<div class="slides"></div>
+				<!-- Controls for the borderless lightbox -->
+				<h3 class="title"></h3>
+				<a class="prev">‹</a>
+				<a class="next">›</a>
+				<a class="close">×</a>
+				<a class="play-pause"></a>
+				<ol class="indicator"></ol>
+				<!-- The modal dialog, which will be used to wrap the lightbox content -->
+				<div class="modal fade">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<button type="button" class="close" aria-hidden="true">&times;</button>
+								<h4 class="modal-title"></h4>
+							</div>
+							<div class="modal-body next"></div>
+							<div class="modal-footer">
+								<button type="button" class="btn btn-default pull-left prev">
+									<i class="glyphicon glyphicon-chevron-left"></i>
+									Previous
+								</button>
+								<button type="button" class="btn btn-primary next">
+									Next
+									<i class="glyphicon glyphicon-chevron-right"></i>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
+			
+			<h4>Gallery</h4>
+			
+			<hr>
+			
+			<div id="links">
+				<a href="images/gallery/collection_niamh.jpg" title="" data-gallery>
+					<img src="images/gallery/collection_niamh_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_11.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_11_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/collection_aoife.jpg" title="" data-gallery>
+					<img src="images/gallery/collection_aoife_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/collection_carol.jpg" title="" data-gallery>
+					<img src="images/gallery/collection_carol_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/collection_louise.jpg" title="" data-gallery>
+					<img src="images/gallery/collection_louise_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_12.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_12_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_13.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_13_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_1.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_1_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_2.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_2_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_3.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_3_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_4.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_4_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_5.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_5_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_6.jpg" data-gallery>
+					<img src="images/gallery/gallery_6_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_7.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_7_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_8.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_8_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_9.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_9_teeny.jpg" alt="">
+				</a>
+				<a href="images/gallery/gallery_10.jpg" title="" data-gallery>
+					<img src="images/gallery/gallery_10_teeny.jpg" alt="">
+				</a>
+			</div>
+			
+			<br>
+			<br>
+			
+			<h4>Collections</h4>
+			
+			<hr>
+			<div id="links">
+				<a href="images/collections/collection_grainne.jpg" title="Grainne" data-gallery>
+					<img src="images/collections/collection_grainne_teeny.jpg" alt="Grainne">
+				</a>
+				<a href="images/collections/collection_clodagh.jpg" title="Clodagh" data-gallery>
+					<img src="images/collections/collection_clodagh_teeny.jpg" alt="Clodagh">
+				</a>
+				<a href="images/collections/collection_aoife_2.jpg" title="Aoife" data-gallery>
+					<img src="images/collections/collection_aoife_2_teeny.jpg" alt="Aoife">
+				</a>
+				<a href="images/collections/collection_grainne_2.jpg" title="Grainne" data-gallery>
+					<img src="images/collections/collection_grainne_2_teeny.jpg" alt="Grainne">
+				</a>
+				<a href="images/collections/collection_louise_2.jpg" title="Louise" data-gallery>
+					<img src="images/collections/collection_louise_2_teeny.jpg" alt="Louise">
+				</a>
+				<a href="images/collections/collection_siobhain.jpg" title="Siobhain" data-gallery>
+					<img src="images/collections/collection_siobhain_teeny.jpg" alt="Siobhain">
+				</a>
+				<a href="images/collections/collection_olivia.jpg" title="Olivia" data-gallery>
+					<img src="images/collections/collection_olivia_teeny.jpg" alt="Olivia">
+				</a>
+				<a href="images/collections/collection_roisin.jpg" title="Roisin" data-gallery>
+					<img src="images/collections/collection_roisin_teeny.jpg" alt="Roisin">
+				</a>
+				<a href="images/collections/collection_eimear.jpg" title="Eimear" data-gallery>
+					<img src="images/collections/collection_eimear_teeny.jpg" alt="Eimear">
+				</a>
+				<a href="images/collections/collection_louise.jpg" title="Louise" data-gallery>
+					<img src="images/collections/collection_louise_teeny.jpg" alt="Louise">
+				</a>
+				<a href="images/collections/collection_aoife.jpg" title="Aoife" data-gallery>
+					<img src="images/collections/collection_aoife_teeny.jpg" alt="Aoife">
+				</a>
+				<a href="images/collections/collection_carol.jpg" title="Carol" data-gallery>
+					<img src="images/collections/collection_carol_teeny.jpg" alt="Carol">
+				</a>
+				<a href="images/collections/collection_niamh.jpg" title="Niamh" data-gallery>
+					<img src="images/collections/collection_niamh_teeny.jpg" alt="Niamh">
+				</a>
+			</div>
+			
+			<br>
+			<br>
+			
+			<h4>Our Brides</h4>
+			
+			<hr>
+			
+			<div id="links">
+				<a href="images/our_brides/our_brides_6.jpg" title="" data-gallery>
+					<img src="images/our_brides/our_brides_6_teeny.jpg" alt="">
+				</a>
+				<a href="images/our_brides/our_brides_7.jpg" title="" data-gallery>
+					<img src="images/our_brides/our_brides_7_teeny.jpg" alt="">
+				</a>
+				<a href="images/our_brides/our_brides_8.jpg" title="" data-gallery>
+					<img src="images/our_brides/our_brides_8_teeny.jpg" alt="">
+				</a>
+				<a href="images/our_brides/our_brides_1.jpg" title="" data-gallery>
+					<img src="images/our_brides/our_brides_1_teeny.jpg" alt="">
+				</a>
+				<a href="images/our_brides/our_brides_2.jpg" title="" data-gallery>
+					<img src="images/our_brides/our_brides_2_teeny.jpg" alt="">
+				</a>
+				<a href="images/our_brides/our_brides_3.jpg" title="" data-gallery>
+					<img src="images/our_brides/our_brides_3_teeny.jpg" alt="">
+				</a>
+				<a href="images/our_brides/our_brides_4.jpg" title="" data-gallery>
+					<img src="images/our_brides/our_brides_4_teeny.jpg" alt="">
+				</a>
+				<a href="images/our_brides/our_brides_5.jpg" title="" data-gallery>
+					<img src="images/our_brides/our_brides_5_teeny.jpg" alt="">
+				</a>
+			</div>
+			
+			<br>
+			<br>
+			
+			<h4>Flowergirl/Communion</h4>
+			
+			<hr>
+			
+			<div id="links">
+				<a href="images/flowergirl_communion/flowergirl_communion_1.jpg" title="" data-gallery>
+					<img src="images/flowergirl_communion/flowergirl_communion_1_teeny.jpg" alt="">
+				</a>
+				<a href="images/flowergirl_communion/flowergirl_communion_2.jpg" title="" data-gallery>
+					<img src="images/flowergirl_communion/flowergirl_communion_2_teeny.jpg" alt="">
+				</a>
+				<a href="images/flowergirl_communion/flowergirl_communion_3.jpg" title="" data-gallery>
+					<img src="images/flowergirl_communion/flowergirl_communion_3_teeny.jpg" alt="">
+				</a>
+				<a href="images/flowergirl_communion/flowergirl_communion_4.jpg" title="" data-gallery>
+					<img src="images/flowergirl_communion/flowergirl_communion_4_teeny.jpg" alt="">
+				</a>
+				<a href="images/flowergirl_communion/flowergirl_communion_5.jpg" title="" data-gallery>
+					<img src="images/flowergirl_communion/flowergirl_communion_5_teeny.jpg" alt="">
+				</a>
+			</div>
+		
+		</div><!-- end core -->
 		
 	</div>
 </body>
--- public/index.php
+++ public/index.php
@@ -3,32 +3,59 @@
 <head>
 	<title>Aideen Cross Designs</title>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-	<link href="css/style.css" rel="stylesheet" type="text/css" />
-	<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Alex+Brush" />
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	
 	<script src="js/image_slider.js" type="text/javascript"></script>
+	
+	<!-- Blueimp image gallery CSS -->
+	<link rel="stylesheet" href="//blueimp.github.io/Gallery/css/blueimp-gallery.min.css">
+	
+	<!-- Image gallery CSS -->
+	<link rel="stylesheet" href="css/bootstrap-image-gallery.min.css">
+	
+	<!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i|Pinyon+Script|Open+Sans|Alex+Brush" rel="stylesheet">
+	
+	<link href="css/style.css" rel="stylesheet" type="text/css" />
+	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	<div class="wrapperBody">
 	
 		<header>
-			<!--<h1>Aideen Cross Designs</h1>-->
-			<div class="banner">
-				<img src="images/logo.jpg" alt=""/>
-			</div>
-			<nav>
-				<ul>
-					<li><a style="text-decoration: underline; color: #ff3399" href="index.php">Home</a></li>
-					<li><a href="gallery.php">Gallery</a></li>
-					<li><a href="collections.php">Collections</a></li>
-					<li><a href="our_brides.php">Our Brides</a></li>
-					<li><a href="flowergirl_communion.php">Flowergirl/Communion</a></li>
-					<li><a href="about.php">Meet Aideen</a></li>
-					<li><a href="contact.php">Contact Us</a></li>
-				</ul>
-			</nav>
-		</header>
+			<nav class="navbar navbar-default navbar-static-top tackNav">
+				<div class="container">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar6">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Aideen Cross Designs <span class="sub-brand">Award Winning Bridal Designer</span></a>
+						
+					</div>
+					<div id="navbar6" class="navbar-collapse collapse">
+						<ul class="nav navbar-nav navbar-right">
+							<li class="active"><a href="#">Home</a></li>
+							<li><a href="about.php">About</a></li>
+							<li><a href="contact.php">Contact</a></li>
+							<li><a href="gallery.php">Gallery</a></li>
 
-		<hr>
+						</ul>
+					</div>
+				</div>
+			</nav>
+			<!--end new nav bar-->
+		</header>
 		
 		<div class="core">
 			<p style="font-size: 110%">
